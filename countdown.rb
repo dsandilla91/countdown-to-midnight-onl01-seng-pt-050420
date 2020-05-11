42afde80accd1527f6e902e14bd5b003df8ddea0
Deborah Santillan <s.debb@yahoo.com>
2020-05-11 01:48:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,15 @@
 def countdown_to_midnight(countdown)
 x = 1
 while countdown > 0 
-end 
   countdown_to_midnight "#{12} SECONDS"
+end 
   "HAPPY NEW YEAR!"
 end
 def countdown_with_sleep (countdown) 
   x = 1
 while countdown < 10
-end
   countdown_with_sleep "sleep(5)"
+end 
   "HAPPY NEW YEAR!"
 end
 end
